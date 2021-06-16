8e01eb6d86785382b90ec3142b88737f34e4d686
ashin <ashin@brokwill.com>
2021-06-16 11:25:19 +0530

    change 4 commit

--- app/src/main/java/com/ashin/findmyphone/MainActivity.kt
+++ app/src/main/java/com/ashin/findmyphone/MainActivity.kt
@@ -9,8 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val userData=UserData(this)
         userData.getPhone()
-        println("hello")
-        println("Experimental")
-        println("how are you")
+
     }
 }
